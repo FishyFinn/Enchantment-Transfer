a84260c11ef135d21ceecb0e0a1f5b77ea6441c1
BuildTools <unconfigured@null.spigotmc.org>
2025-04-27 01:27:16 +1000

    refactored Variable names for clarity allowed return item to drop if players inventory is full added ability to move a single enchant to a blank book allowed player to place a whole stack of blank books in input for mass enchantment removal

--- src/main/java/com/treekicker4/enchantmenttransfer/events/AnvilEvents.java
+++ src/main/java/com/treekicker4/enchantmenttransfer/events/AnvilEvents.java
@@ -1,12 +1,10 @@
 package com.treekicker4.enchantmenttransfer.events;
 import com.treekicker4.enchantmenttransfer.EnchantmentTransfer;
 import com.treekicker4.enchantmenttransfer.core.EnchantmentTransferConfig;
-import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.entity.player.AnvilRepairEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,113 +17,57 @@ import java.util.Map;
 public class AnvilEvents {
     @SubscribeEvent
     public static void giveEnchantedBook(AnvilUpdateEvent event) {
-        int i;
-        if (event.getLeft().isEnchanted() && event.getRight().getItem() == Items.BOOK && event.getRight().getCount() == 1) {
-            //if (event.getRight().isEnchanted() && event.getLeft().getItem() == Items.BOOK) {
-            Map<Enchantment, Integer> raw_enchantments = EnchantmentHelper.getEnchantments(event.getLeft());
-            Map<Enchantment, Integer> enchantments = new HashMap<>();
-            int count = 0;
-            for (Map.Entry<Enchantment, Integer> set : raw_enchantments.entrySet()) {
-                if (count >= EnchantmentTransferConfig.limit_value.get()) {
-                    break;
-                }
-                Enchantment enchantment = set.getKey();
-                enchantments.put(enchantment, set.getValue());
-                count++;
-            }
+        //If player is trying to move enchants from item to empty book
+        if (event.getLeft().isEnchanted() && event.getRight().getItem() == Items.BOOK) {
+            Map<Enchantment, Integer> leftItemEnchantments = EnchantmentHelper.getEnchantments(event.getLeft());
+            Map<Enchantment, Integer> finalBookEnchantments = new HashMap<>();
             ItemStack finalBook = new ItemStack(Items.ENCHANTED_BOOK);
-            for (Map.Entry<Enchantment, Integer> set : enchantments.entrySet()) {
-                if (count >= EnchantmentTransferConfig.limit_value.get()) {
-                    break;
-                }
-                Enchantment enchantment = set.getKey();
-                EnchantedBookItem.addEnchantment(finalBook, new EnchantmentInstance(enchantment, set.getValue()));
-                count++;
-            }
-            EnchantmentHelper.setEnchantments(enchantments, finalBook);
-            //event.setOutput(finalBook);
-            ItemStack itemstack = event.getLeft();
-            event.setCost(1);
-            i = 0;
-            ItemStack itemstack1 = itemstack.copy();
-            ItemStack itemstack2 = event.getRight().copy();
-            Map<Enchantment, Integer> map = enchantments;
-            EnchantmentHelper.setEnchantments(map, itemstack2);
-            boolean flag = true;
-            if (!itemstack2.isEmpty()) {
-                //flag = itemstack2.getItem() == Items.ENCHANTED_BOOK && !EnchantedBookItem.getEnchantments(itemstack2).isEmpty();
-                Map<Enchantment, Integer> map1 = enchantments;
-                count = 0;
-                for (Enchantment enchantment1 : map1.keySet()) {
-                    if (enchantment1 != null) {
-                        if (count >= EnchantmentTransferConfig.limit_value.get()) {
-                            break;
-                        }
-                        count++;
-                        int i2 = map.getOrDefault(enchantment1, 0);
-                        int j2 = map1.get(enchantment1);
-                        j2 = i2 == j2 ? j2 + 1 : Math.max(j2, i2);
-                        boolean flag1 = true;
-                        for (Enchantment enchantment : map.keySet()) {
-                            if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment)) {
-                                flag1 = false;
-                                ++i;
-                            }
-                        }
 
-                        if (flag1) {
-                            if (j2 > enchantment1.getMaxLevel()) {
-                                j2 = enchantment1.getMaxLevel();
-                            }
-
-                            map.put(enchantment1, j2);
-                            int k3 = 0;
-                            switch (enchantment1.getRarity()) {
-                                case COMMON:
-                                    k3 = 1;
-                                    break;
-                                case UNCOMMON:
-                                    k3 = 2;
-                                    break;
-                                case RARE:
-                                    k3 = 4;
-                                    break;
-                                case VERY_RARE:
-                                    k3 = 8;
-                            }
-
-                            if (flag) {
-                                k3 = Math.max(1, k3 / 2);
-                            }
-
-                            i += k3 * j2;
-                            if (itemstack.getCount() > 1) {
-                                i = 40;
-                            }
-                        }
+            int enchantmentLimit = EnchantmentTransferConfig.limit_value.get();
+            if (leftItemEnchantments.size() > enchantmentLimit) {
+                int count = 0;
+                for (Map.Entry<Enchantment, Integer> originalEnchantmentEntry : leftItemEnchantments.entrySet()) {
+                    if (count >= enchantmentLimit) {
+                        break;
                     }
-                }
-            }
-            if (EnchantmentTransferConfig.fixed_value.get() != 1000) {
-                if (EnchantmentTransferConfig.fixed_value.get() == 0) {
-                    event.setCost(1);
-                } else {
-                    event.setCost(EnchantmentTransferConfig.fixed_value.get());
+                    finalBookEnchantments.put(originalEnchantmentEntry.getKey(), originalEnchantmentEntry.getValue());
+                    count++;
                 }
             } else {
-                double factor_value = EnchantmentTransferConfig.factor_value.get();
-                if (factor_value > 0.0) {
-                    event.setCost((int) Math.round(i * factor_value));
-                } else {
-                    event.setCost(1);
-                }
+                EnchantmentHelper.setEnchantments(finalBookEnchantments, finalBook);
+            }
+            event.setCost(EnchantmentTransfer.transferCost(leftItemEnchantments));
+            event.setOutput(finalBook);
+            event.setMaterialCost(1);
+            return;
+        }
+
+        //If player is trying to move 1 enchant from enchanted to a blank book
+        if (event.getLeft().getItem() == Items.ENCHANTED_BOOK && event.getRight().getItem() == Items.BOOK) {
+            Map<Enchantment, Integer> bookEnchantments = EnchantmentHelper.getEnchantments(event.getLeft());
+
+            Map<Enchantment, Integer> finalBookEnchantments = new HashMap<>();
+            ItemStack finalBook = new ItemStack(Items.ENCHANTED_BOOK);
+
+            //add the first enchantment from the entry to the finalBookEnchantments
+            if (EnchantmentTransferConfig.limit_value.get() > 0 && !bookEnchantments.isEmpty()) {
+                Map.Entry<Enchantment, Integer> firstEntry = bookEnchantments.entrySet().iterator().next();
+                finalBookEnchantments.put(firstEntry.getKey(), firstEntry.getValue());
             }
 
+            EnchantmentHelper.setEnchantments(finalBookEnchantments, finalBook);
+            event.setCost(EnchantmentTransfer.transferCost());
             event.setOutput(finalBook);
+            event.setMaterialCost(1);
+            return;
         }
+
+
     }
+
     @SubscribeEvent
     public static void giveItemBack(AnvilRepairEvent event) {
+        //If player is trying to move enchants from item to empty book
         if (event.getLeft().isEnchanted() && event.getRight().getItem() == Items.BOOK) {
             if (EnchantmentTransferConfig.return_value.get() != 0) {
                 ItemStack disenchanted = event.getLeft().copy();
@@ -133,7 +75,27 @@ public class AnvilEvents {
                 if (EnchantmentTransferConfig.fixed_value.get() == 0) {
                     event.getEntity().giveExperienceLevels(1);
                 }
-                event.getEntity().getInventory().add(disenchanted);
+                if(!event.getEntity().getInventory().add(disenchanted))
+                {
+                    event.getEntity().drop(disenchanted, false);
+                }
+            }
+        }
+        //If player is trying to move 1 enchant from enchanted to a blank book
+        if (event.getLeft().getItem() == Items.ENCHANTED_BOOK && event.getRight().getItem() == Items.BOOK) {
+            if (EnchantmentTransferConfig.return_value.get() != 0) {
+                ItemStack originalBook = event.getLeft().copy();
+                Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(originalBook);
+                Map.Entry<Enchantment, Integer> firstEnchantment = EnchantmentHelper.getEnchantments(originalBook).entrySet().iterator().next();
+                enchantments.remove(firstEnchantment.getKey(), firstEnchantment.getValue());
+
+                if (EnchantmentTransferConfig.fixed_value.get() == 0) {
+                    event.getEntity().giveExperienceLevels(1);
+                }
+                if(!event.getEntity().getInventory().add(originalBook))
+                {
+                    event.getEntity().drop(originalBook, false);
+                }
             }
         }
     }
